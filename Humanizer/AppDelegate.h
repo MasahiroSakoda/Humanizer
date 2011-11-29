25f1b0b9a91e818b6afb7b45c8cf73e0aea68144
MasahiroSakoda <thunderbird.m.s@gmail.com>
2011-11-29 20:10:12 +0900

    Initial Commit

---
+++ Humanizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Humanizer
+//
+//  Created by 迫田 雅弘 on 11/11/29.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Humanizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Humanizer
+//
+//  Created by 迫田 雅弘 on 11/11/29.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
